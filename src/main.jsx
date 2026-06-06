459a594feb40e8acc09659a60ab1bbedb90a396a
Promessa LP <iaplagodospeixes@gmail.com>
2026-06-06 16:29:45 -0300

    fix store jsx extension

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { StoreProvider } from './lib/store.js'
+import { StoreProvider } from './lib/store.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
